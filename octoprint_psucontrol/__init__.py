dcd5de6ac6347fc2d2d14f28ffc40b2ec7527c56
Ken <Ken@Kens-MacBook-Pro.local>
2017-08-06 08:52:48 -0400

    Testing automatic PSU turn on when OctoPrint server starts up

--- octoprint_psucontrol/__init__.py
+++ octoprint_psucontrol/__init__.py
@@ -145,6 +145,8 @@ class PSUControl(octoprint.plugin.StartupPlugin,
 
         self._start_idle_timer()
 
+        self.turn_psu_on()
+
     def _gpio_board_to_bcm(self, pin):
         if GPIO.RPI_REVISION == 1:
             pin_to_gpio = self._pin_to_gpio_rev1
