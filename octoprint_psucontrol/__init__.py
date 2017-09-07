ca1718495b061100557c155dfc51b9f6291ff633
Ken <Ken@Kens-MacBook-Pro.local>
2017-09-06 22:32:00 -0400

    Revert "Testing autoOnServerBoot"
    This reverts commit c46c8df54545ac6f35b7e1eb2196f5ff9ab0f33e.

--- octoprint_psucontrol/__init__.py
+++ octoprint_psucontrol/__init__.py
@@ -38,7 +38,6 @@ class PSUControl(octoprint.plugin.StartupPlugin,
         self.pseudoOffGCodeCommand = ''
         self.postOnDelay = 0.0
         self.autoOn = False
-		self.autoOnServerBoot = False
         self.autoOnTriggerGCodeCommands = ''
         self._autoOnTriggerGCodeCommandsArray = []
         self.enablePowerOffWarningDialog = True
@@ -119,9 +118,6 @@ class PSUControl(octoprint.plugin.StartupPlugin,
         self.autoOn = self._settings.get_boolean(["autoOn"])
         self._logger.debug("autoOn: %s" % self.autoOn)
 
-        self.autoOnServerBoot = self._settings.get_boolean(["autoOnServerBoot"])
-        self._logger.debug("autoOnServerBoot: %s" % self.autoOnServerBoot)
-
         self.autoOnTriggerGCodeCommands = self._settings.get(["autoOnTriggerGCodeCommands"])
         self._autoOnTriggerGCodeCommandsArray = self.autoOnTriggerGCodeCommands.split(',')
         self._logger.debug("autoOnTriggerGCodeCommands: %s" % self.autoOnTriggerGCodeCommands)
@@ -148,10 +144,6 @@ class PSUControl(octoprint.plugin.StartupPlugin,
         self._checkPSUTimer.start()
 
         self._start_idle_timer()
-			
-        if self.autoOn == 1 && self.autoOnServerBoot == 1
-            self._logger.debug("Switching PSU On Using Auto On Server Boot")
-            self.turn_psu_on()
 
     def _gpio_board_to_bcm(self, pin):
         if GPIO.RPI_REVISION == 1:
@@ -472,7 +464,6 @@ class PSUControl(octoprint.plugin.StartupPlugin,
             invertsenseGPIOPin = False,
             senseGPIOPinPUD = '',
             autoOn = False,
-            autoOnServerBoot = False,
             autoOnTriggerGCodeCommands = "G0,G1,G2,G3,G10,G11,G28,G29,G32,M104,M106,M109,M140,M190",
             enablePowerOffWarningDialog = True,
             powerOffWhenIdle = False,
@@ -510,7 +501,6 @@ class PSUControl(octoprint.plugin.StartupPlugin,
         self.invertsenseGPIOPin = self._settings.get_boolean(["invertsenseGPIOPin"])
         self.senseGPIOPinPUD = self._settings.get(["senseGPIOPinPUD"])
         self.autoOn = self._settings.get_boolean(["autoOn"])
-        self.autoOnServerBoot = self._settings.get_boolean(["autoOnServerBoot"])
         self.autoOnTriggerGCodeCommands = self._settings.get(["autoOnTriggerGCodeCommands"])
         self._autoOnTriggerGCodeCommandsArray = self.autoOnTriggerGCodeCommands.split(',')
         self.powerOffWhenIdle = self._settings.get_boolean(["powerOffWhenIdle"])
